2381ac5b78c6429b7288faaae5379969932c961b
ArkesDev <118545177+ArkesDev@users.noreply.github.com>
2022-11-18 15:48:00 +0100

    Update algoritm.py

--- algoritm.py
+++ algoritm.py
@@ -1,13 +1,13 @@
 
-frase=input("Dimmi una frase: ")
-print(frase.split())
-print(len(frase.split()))
-conta_parole=0
-ex_lettera="S"
-for lettera in frase:
-    if conta_parole==0:
-        conta_parole=+1
-        if lettera == " "and lettera !=ex_lettera :
-                conta_parole= conta_parole+1
-        ex_lettera=lettera
-print("Questa frase contine", conta_parole, "parole")
+sentence=input("Tell me a sentence: ")
+print(sentence.split())
+print(len(sentence.split()))
+number_words=0
+ex_words="S"
+for words in sentence:
+    if number_words==0:
+        number_words=+1
+        if words == " "and words !=ex_words :
+                number_words= number_words+1
+        ex_words=words
+print("This sentence contains", number_words, "this words!")
